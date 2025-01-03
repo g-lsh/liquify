997e88a1d483ff5f815802c056b845fef99af484
ghislain <g.lesergeant@gmail.com>
2025-01-03 16:24:56 +0100

    only convert template for liquid

--- index.mjs
+++ index.mjs
@@ -22,6 +22,34 @@ function replaceFirstAndLast(str, firstChar, lastChar) {
     return firstChar + str.slice(1, -1) + lastChar;
 }
 
+function convertTemplate(template) {
+    let convertedTemplate = template
+    convertedTemplate = template.replace(/""/g, '"');
+
+    // Fix currentUrl variable
+    convertedTemplate = convertedTemplate.replace(/\$currentUrl/g,'_currentUrl');
+
+    // Fix elif statement
+    convertedTemplate = convertedTemplate.replace(/\elif/g,'elseif');
+
+    // Fix set statement
+    convertedTemplate = convertedTemplate.replace(/ set /g,' assign ');
+
+    // Fix replace filter
+    convertedTemplate = convertedTemplate.replace(/replace\((.*?),\s*(.*?)\)/g, "replace: $1, $2")
+
+    // Fix truncate filter
+    convertedTemplate = convertedTemplate.replace(/truncate\((\d+)\)/g, "truncate: $1");
+
+    // Fix length => size
+    convertedTemplate = convertedTemplate.replace(/\|length/g, "|size");
+
+    // Fix trim
+    convertedTemplate = convertedTemplate.replace(/\|trim/g, "|strip");
+
+    return convertedTemplate
+}
+
 function parseCSV(csv) {
     const lines = csv.split('\n');
     const headers = lines[0].split(',');
@@ -39,12 +67,6 @@ function parseCSV(csv) {
         // extract config column into details
         let details = currentLine[5]
         details = details.replace(/""/g, '"');
-        /*details = details.replace(/\$currentUrl/g,'_currentUrl');
-details = details.replace(/\elif/g,'elseif');
-        details = details.replace(/ set /g,' assign ');
-        details = details.replace(/replace\((.*?),\s*(.*?)\)/g, "replace: $1, $2")
-                details = details.replace(/replace \((.*?),\s*(.*?)\)/g, "replace: $1, $2")
-*/
         obj.details = details.trim();
         obj.details = replaceFirstAndLast(obj.details, "", "");
 
@@ -86,6 +108,10 @@ fs.readFile(filePath, 'utf8', (err, file) => {
         var failNunjucks = 0;
         var successNunjucks = 0;
         var successLiquid = 0;
+        var failTruncate = 0;
+        var failReplace = 0;
+        var failTrim = 0;
+        var failLength = 0;
         var csv = parseCSV(file);
         var totalTemplates = 0;
         csv.forEach(datum => {
@@ -95,7 +121,8 @@ fs.readFile(filePath, 'utf8', (err, file) => {
                     "description": "A wbesite description",
                     "title": "A website title",
                     "h1": "A nice header",
-                    "canonical": "https://example.Com/canonical"
+                    "canonical": "https://example.Com/canonical",
+                    "metadesc": "test"
                 };
                 try {
                     var conf = datum.details;
@@ -103,23 +130,42 @@ fs.readFile(filePath, 'utf8', (err, file) => {
                     acts.forEach(act => {
                             var template = act.transformationConfig.template;
                             totalTemplates++
+                            const convertedTemplate = convertTemplate(template)
                             try {
-                                checkLiquid(datum.website_id, datum.id, template, data);
+                                checkLiquid(datum.website_id, datum.id, convertedTemplate, data);
                                 successLiquid++
                             } catch (e) {
                                 if (e.message.includes("$currentUrl")) {
                                     failCurrentUrl++;
                                 } else if (e.message.includes("tag \"elif\"")) {
+                                    console.warn("LIQUID LIQUID ERROR", e, convertedTemplate);
                                     failElif++;
                                 } else if (e.message.includes("tag \"set\"")) {
-                                    console.log(chalk.green(template));
+                                    console.warn("LIQUID SET ERROR", e, convertedTemplate);
                                     failSet++;
                                 } else if (e.message.includes("expected \":\" after filter name")) {
                                     failFilter++;
-                                    console.warn("LIQUID ERROR", e, template);
+
+                                    if (convertedTemplate.includes("replace")) {
+                                        failReplace++;
+                                    }
+
+                                    if (convertedTemplate.includes("truncate")) {
+                                        failTruncate++;
+                                    }
+
+                                    if (convertedTemplate.includes("trim")) {
+                                        failTrim++;
+                                    }
+
+                                    if (convertedTemplate.includes("|length") || convertedTemplate.includes("| length")) {
+                                        failLength++;
+                                    }
+
+                                    console.warn("LIQUID FILTER ERROR", e, convertedTemplate);
                                 } else {
                                     failedOther++;
-                                    console.warn("LIQUID ERROR", e, template);
+                                    // console.warn("LIQUID ERROR", e, template);
                                 }
                                 failLiquid++;
                                 results[datum.website_id] = results[datum.website_id] ?? {total:0};
@@ -144,15 +190,15 @@ fs.readFile(filePath, 'utf8', (err, file) => {
             }
         );
 
-        console.log(chalk.bgYellow("                    "));
+        console.log(chalk.bgWhiteBright("                    "));
         console.log("RESULTS")
-        console.log(chalk.bgYellow("                    "));
+        console.log(chalk.bgWhiteBright("                    "));
         console.log("TOTAL TEMPLATES: ", totalTemplates);
-        console.log(chalk.bgYellow("                    "));
+        console.log(chalk.bgWhiteBright("                    "));
         console.log(chalk.cyan("NUNJUCKS"));
         console.log("FAILED: ", failNunjucks);
         console.log("SUCCESS: ", successNunjucks);
-        console.log(chalk.bgYellow("                    "));
+        console.log(chalk.bgWhiteBright("                    "));
         console.log(chalk.blue("LIQUID"));
         console.log("SUCCESS: ", successLiquid);
         console.log("FAILED TOTAL:", failLiquid);
@@ -160,10 +206,14 @@ fs.readFile(filePath, 'utf8', (err, file) => {
         console.log("FAILED because of elif", failElif);
         console.log("FAILED because of set", failSet);
         console.log("FAILED because of filter", failFilter);
+        console.log("FAILED because of truncate filter: ", failTruncate);
+        console.log("FAILED because of replace filter: ", failReplace);
+        console.log("FAILED because of trim filter: ", failTrim);
+        console.log("FAILED because of length filter: ", failLength);
         console.log("FAILED because of other", failedOther);
-        console.log(chalk.bgYellow("                    "));
+        console.log(chalk.bgWhiteBright("                    "));
         console.log(chalk.red("FAILED UNKNOWN REASON"), a);
-        console.log(chalk.bgYellow("                    "));
+        console.log(chalk.bgWhiteBright("                    "));
         // console.log(chalk.gray("INFO"));
         // console.log(results)
     }
